6223a6961ccffdeb2040a891d9184533fc0193e8
Maxishoo <rcmaksim@gmail.com>
2023-09-11 22:03:59 +0300

    xmmm

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-a = int(input())
-b = int(input())
+n = int(input())
+m = int(input())
 
-mi = min(a, b)
-ma = max(a, b)
-c = 0
-i = 0
-while ma > 0:
-    if mi > 0:
-        c += (ma % 10 + mi % 10) % 10 * 10 ** i
-        mi = mi // 10
-    else:
-        c += ma % 10 * 10 ** i
-    ma = ma // 10
-    i += 1
-print(c)
+t = int(input())
+print((n + t // 60) % 24)
+print(f"{(n + t // 60) % 24:0>2}:{(m + t % 60) % 60:0>2}")
